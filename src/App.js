3721168907c4754ba00834daf8d8fff1557af336
Basile Raiwet <basileraiwet.pro@gmail.com>
2020-04-21 14:58:20 +0200

    add react-router system

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import Search from './components/Search'
+import Search from './components/Search';
+import {Switch, Route} from 'react-router-dom';
+import GameList from './components/GamesList'
 
 function App() {
   return (
-    <Search />
+    <Switch>
+      <Route exact path="/">
+        <Search />
+      </Route>
+      <Route path="/search" component={GameList} />
+    </Switch>
   )
 }
 
---
+++ src/components/GamesList.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const GameList = () => {
+    return(
+        <h1>Hello world</h1>
+    )
+}
+
+export default GameList
