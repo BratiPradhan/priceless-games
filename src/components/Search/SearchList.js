f78470549808ee06014287fa0153eaaf0a489b2a
Basile Raiwet <basileraiwet.pro@gmail.com>
2020-04-27 22:50:59 +0200

    creating loader spin component

--- src/components/Search/SearchList.js
+++ src/components/Search/SearchList.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 import SearchResult from './SearchResult';
 
 const SearchList = (props) => {
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
