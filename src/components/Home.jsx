01acddc0cb1d5c3de72adbd0a41da379000d5895
Basile Raiwet <basileraiwet.pro@gmail.com>
2020-05-07 13:11:37 +0200

    banner design improvement 1/2

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,8 +3,20 @@ import Search from "./Search/Search"
 
 const Home = () => {
     return(
-        <div className="home-container">
-            <Search />
+        <div className="home">
+            <section className="banner">
+                <div className="container">
+                    <div className='banner-elem'>
+                        <div className="banner-text">
+                            <h1>Find your game at the best price</h1>
+                            <Search />
+                        </div>
+                    </div>
+                    <div className="banner-elem">
+
+                    </div>
+                </div>
+            </section>
         </div>
     )
 }
