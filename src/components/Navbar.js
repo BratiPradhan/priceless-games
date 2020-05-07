cb863c740dc59179af8d3cb6f196650e898601b1
Basile Raiwet <basileraiwet.pro@gmail.com>
2020-05-07 11:39:55 +0200

    refactoring routes with render props

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,8 +4,9 @@ import {Link} from 'react-router-dom';
 const Navbar = () => {
     return(
         <nav className="main-nav">
-            <Link to="/">Home</Link>
+            <Link to="/">Search</Link>
             <Link to="/deals">Bests deals</Link>
+            <Link to="/new-games">New Games</Link>
         </nav>
     )
 }
