9095e5fb005a98cd60ae9c076926d5005bae546c
Basile Raiwet <basileraiwet.pro@gmail.com>
2020-05-07 11:54:48 +0200

    add react icon package and bell to the nav for future notification purpose"

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {Link} from 'react-router-dom';
+import {FaBell} from 'react-icons/fa'
 
 const Navbar = () => {
     return(
@@ -8,6 +9,9 @@ const Navbar = () => {
                 <Link to="/">Search</Link>
                 <Link to="/deals">Bests deals</Link>
                 <Link to="/new-games">New Games</Link>
+                <div className='nav-alert'>
+                    <Link to='/'><FaBell /></Link>
+                </div>
             </nav>
         </header>
     )
