6936fa39969ec449d59091a9c87712b36dfbb357
Tiesco2 <dtiekoura@yahoo.fr>
2020-05-07 17:22:07 +0200

    create select.js

--- src/components/BestDeals/Deals.js
+++ src/components/BestDeals/Deals.js
@@ -3,23 +3,37 @@ import axios from 'axios';
 import DealCard from './DealCard'
 import LoaderSpin from '../Loader'
 import './Deals.css'
+import Select from './Select'
 
 class Deals extends Component {
     constructor(props){
         super(props);
         this.state = {
             deals: [],
-            isLoading: false
+            isLoading: false, 
+            sortBy: 'Reviews'
         }
     }
 
     componentDidMount = () => {
         this.getDeals();
     }
-
+    componentDidUpdate(prevProps, prevState) {
+        if(prevState.sortBy !== this.state.sortBy){
+            this.getDeals();
+        }
+       
+    }
+    handleSelectedFilter = (event) =>{
+        console.log(event.target.value)
+        this.setState({
+            sortBy: event.target.value
+        })
+        
+    }
     getDeals = () => {
         this.setState({isLoading: true}, () => {
-            axios.get('https://www.cheapshark.com/api/1.0/deals?pageSize=20&sortBy=Reviews')
+            axios.get(`https://www.cheapshark.com/api/1.0/deals?pageSize=20&sortBy=${this.state.sortBy}`)
             .then(res => res.data)
             .then(data => {
                 const deals = data;
@@ -35,11 +49,13 @@ class Deals extends Component {
     render(){
         const { deals, isLoading } = this.state;
         return(
-            <div className="game-list-rows">          
+            <div className="game-list-rows">   
+               <Select handleSelectedFilter={this.handleSelectedFilter} />       
                 {isLoading
                     ? <LoaderSpin />
                     : deals.map((deal, i) => <DealCard key={i} {...deal} />)
                 }
+             
             </div>
         )
     }
