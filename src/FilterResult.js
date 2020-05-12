ac730f0c511e91443118abf36301d55788b80db7
Tiesco2 <dtiekoura@yahoo.fr>
2020-05-12 11:10:25 +0200

    create file Filterresult

---
+++ src/FilterResult.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class FilterResult extends Component {
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default FilterResult
