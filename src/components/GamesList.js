c79ca676940b8d48448dd27583eb57ba4905b334
Tiesco2 <dtiekoura@yahoo.fr>
2020-05-12 23:49:32 +0200

    create component SortedPrice in Components and import it in GamesList

--- src/components/GamesList.js
+++ src/components/GamesList.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react'
 import GameCard from './GameCard';
+import SortedPrice from './SortedPrice';
 
 class GameList extends Component{
+    state = {
+        games: this.props.location.state.games
+    }
     render(){
         const games = this.props.location.state.games
-        games.sort((a,b) => a.cheapest-b.cheapest)  
+       // const byPrice = (a,b) => a.cheapest-b.cheapest
+        //games.sort(byPrice) 
+
         return(
             <div className="game-list-rows">
                 {
-                games.map(game => 
+                this.state.games.map(game => 
                     <GameCard
                         key={game.gameID}
                         {...game}
                     />)
-                }
+                    
+                } 
+               <SortedPrice />
             </div>
         )
     }
---
+++ src/components/SortedPrice.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SortedPrice = (props) => {
+    return(
+        <div>
+             <select id="cars">
+                    <option value="cheapest">Price</option>
+                    <option value="">Pertinent</option>
+                </select>  
+
+        </div>
+    )
+}
+export default SortedPrice;
