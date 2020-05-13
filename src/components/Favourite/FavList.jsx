f448ec33c5cc7c107f9a731eded4ff9d44daa4f5
Basile Raiwet <basileraiwet.pro@gmail.com>
2020-05-13 21:56:06 +0200

    add banner to favlist

--- src/components/Favourite/FavList.jsx
+++ src/components/Favourite/FavList.jsx
@@ -5,11 +5,27 @@ import './FavList.css'
 const FavList = ({removeFav, favGames}) => {
     
     return(
-        <div className='container'>
+        <>
+        <section className="banner">
+                <div className="container banner-container">
+                    <div className='banner-elem'>
+                        <div className="banner-text">
+                            <h1>Find your game at the best price</h1>
+                            <p>Search between 25+ game platform, including <span>Steam</span>, <span>Origin</span> and the <span>EpicGameStore</span> .</p>
+                            <Search />
+                        </div>
+                    </div>
+                    <div className="banner-elem img-container">
+                        <img className='banner-img' src={logo} />
+                    </div>
+                </div>
+            </section>
+            <div className='container'>
             <div className='fav-list'>
                 {favGames.map(game => <FavGame key={game.id} removeFav={removeFav}  {...game} />)}
             </div>
-        </div>
+         </div>
+         </>
     )
 }
 
