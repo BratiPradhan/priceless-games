96c497b3aabc6dbf7bd38170d3cbf68e75f40f72
Basile Raiwet <basileraiwet.pro@gmail.com>
2020-05-13 22:07:38 +0200

    add some contet

--- src/components/Favourite/FavList.jsx
+++ src/components/Favourite/FavList.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import FavGame from './FavGame'
 import './FavList.css'
+import logo from '../../imgs/logo-fav.svg'
 
 const FavList = ({removeFav, favGames}) => {
     
     return(
         <>
-        <section className="banner">
+            <section className="banner">
                 <div className="container banner-container">
                     <div className='banner-elem'>
                         <div className="banner-text">
-                            <h1>Find your game at the best price</h1>
-                            <p>Search between 25+ game platform, including <span>Steam</span>, <span>Origin</span> and the <span>EpicGameStore</span> .</p>
-                            <Search />
+                            <h1>Follow your favourite games</h1>
+                            <p>Be notified when the price of one of your favourite game drop !</p>
                         </div>
                     </div>
                     <div className="banner-elem img-container">
@@ -21,10 +21,10 @@ const FavList = ({removeFav, favGames}) => {
                 </div>
             </section>
             <div className='container'>
-            <div className='fav-list'>
-                {favGames.map(game => <FavGame key={game.id} removeFav={removeFav}  {...game} />)}
+                <div className='fav-list'>
+                    {favGames.map(game => <FavGame key={game.id} removeFav={removeFav}  {...game} />)}
+                </div>
             </div>
-         </div>
          </>
     )
 }
